af72d1e71bb1e4673a518ea4c5da4dfec3ab2625
zrrz <247076126@qq.com>
2022-01-21 12:36:44 +0800

    33

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-import { transformSync } from '@babel/core';
+// import { transformSync } from '@babel/core';
+const { transformSync } = require('@babel/core');
 // import generator from "@babel/generator";
 
 const genCode = () => {
     return (t) => {
         return {
             visitor: {
+                ObjectExpression(path) {
+                    console.log(path);
+                    // const treeNode = t.newExpression(t.identifier("TreeNode")
 
+                    // );
+                    // treeNode.arguments
+                }
             }
         };
     };
 };
-
+// t.newExpression(
+//     t.identifier("TreeNode"), [
+//     t.numericLiteral(1),
+//     t.newExpression(t.identifier("TreeNode"), [
+//         t.numericLiteral(2)]
+//     ),
+//     t.newExpression(t.identifier("TreeNode"), [
+//         t.numericLiteral(3)]
+//     )]
+// )
 
 const c = transformSync(
     `
